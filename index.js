bb5f892d11c1618a0eea269a3d2f6573cef42be0
Mauricio Lahitte <mauricio.mm94@gmail.com>
2022-08-24 15:21:50 -0300

    mqtt added

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 const SerialPort = require("serialport").SerialPort;
 const ReadlineParser = require("serialport").ReadlineParser;
-const port = new SerialPort({ baudRate: 9600, path: "COM4" });
-const parser = port.pipe(new ReadlineParser({ delimiter: "\n" })); // Read the port data
+
+const mqtt = require("mqtt");
+const client = mqtt.connect("mqtt://143.198.182.161:1883");
 
 let lastX = 90;
 let lastY = 90;
 let lastPOT = 0;
 
-port.on("open", () => {
-  console.log("serial port open");
-});
-parser.on("data", (data) => {
-  const [X, Y, POT] = data.split("|");
-  if (X != lastX) {
-    console.log("🚀 ~ file: index.js ~ line 16 ~ parser.on ~ X", X);
-    lastX = X;
-  }
-  if (Y != lastY) {
-    console.log("🚀 ~ file: index.js ~ line 16 ~ parser.on ~ Y", Y);    
-    lastY = Y;
-  }
-  if (POT != lastPOT) {
-    console.log("🚀 ~ file: index.js ~ line 16 ~ parser.on ~ POT", POT);    
-    lastPOT = POT;
-  }
+client.on("connect", function () {
+  console.log("mqtt server conected");
+  const port = new SerialPort({ baudRate: 9600, path: "COM4" });
+  const parser = port.pipe(new ReadlineParser({ delimiter: "\n" })); // Read the port data
+
+  port.on("open", () => {
+    console.log("serial port open");
+  });
+  parser.on("data", (data) => {
+    const [X, Y, POT] = data.split("|");
+    
+    if (X != lastX) {
+      console.log("🚀 X", X);
+      client.publish('/FELIA/ROLL', X);
+      lastX = X;
+    }
+    if (Y != lastY) {
+      console.log("🚀 Y", Y);
+      client.publish('/FELIA/YAW', Y);
+      lastY = Y;
+    }
+    if (POT != lastPOT) {
+      console.log("🚀 POT", POT);
+      client.publish('/FELIA/ESC', POT);
+      lastPOT = POT;
+    }
+  });
+  
 });
