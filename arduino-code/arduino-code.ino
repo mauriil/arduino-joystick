3e0038dcd17f4f1a8b139fe696746f4f5e0012e9
Mauricio Lahitte <mauricio.mm94@gmail.com>
2022-08-29 16:09:25 -0300

    adding more controls to arduino

--- arduino-code/arduino-code.ino
+++ arduino-code/arduino-code.ino
@@ -1,40 +1,98 @@
-int VRx = A0;
-int VRy = A1;
-int POT = A2;
-int SW = 2;
+int VRx_1 = A0;
+int VRy_1 = A1;
+int VRx_2 = A2;
+int VRy_2 = A3;
+int VRx_3 = A4;
+int VRy_3 = A5;
 
-int xPosition = 0;
-int yPosition = 0;
-int SW_state = 0;
-int mapX = 0;
-int mapY = 0;
-int mapPOT = 0;
+int POT_1 = A6;
+int POT_2 = A7;
+int POT_3 = A8;
+int POT_4 = A8;
+
+int toggle_1 = 2;
+int toggle_2 = 3;
+int toggle_3 = 4;
+int toggle_4 = 5;
+int toggle_5 = 6;
+int toggle_6 = 7;
+int toggle_7 = 8;
+int toggle_8 = 9;
+int toggle_9 = 10;
+
 
 void setup() {
   Serial.begin(9600); 
   
-  pinMode(VRx, INPUT);
-  pinMode(VRy, INPUT);
-  pinMode(POT, INPUT);
-  pinMode(SW, INPUT_PULLUP); 
+  pinMode(VRx_1, INPUT);
+  pinMode(VRy_1, INPUT);
+  pinMode(VRx_2, INPUT);
+  pinMode(VRy_2, INPUT);
+  pinMode(VRx_3, INPUT);
+  pinMode(VRy_3, INPUT);
+  pinMode(POT_1, INPUT);
+  pinMode(POT_2, INPUT);
+  pinMode(POT_3, INPUT);
+  pinMode(POT_4, INPUT);
+  pinMode(toggle_1, INPUT_PULLUP);
+  pinMode(toggle_2, INPUT_PULLUP);
+  pinMode(toggle_3, INPUT_PULLUP);
+  pinMode(toggle_4, INPUT_PULLUP);
+  pinMode(toggle_5, INPUT_PULLUP);
+  pinMode(toggle_6, INPUT_PULLUP);
+  pinMode(toggle_7, INPUT_PULLUP);
+  pinMode(toggle_8, INPUT_PULLUP);
+  pinMode(toggle_9, INPUT_PULLUP);
   
 }
 
-void loop() {
-  xPosition = analogRead(VRx);
-  yPosition = analogRead(VRy);
-  SW_state = digitalRead(SW);
-  mapX = map(xPosition, 0, 1023, 0, 180);
-  mapY = map(yPosition, 0, 1023, 0, 180);
-  mapPOT = map(analogRead(POT), 0, 678, 0, 180);
-  
-  Serial.print(mapX);
+void loop() {  
+  Serial.print(map(analogRead(VRx_1), 0, 1023, 0, 180));
   Serial.print("|");
-  Serial.print(mapY);
+  Serial.print(map(analogRead(VRy_1), 0, 1023, 0, 180));
   Serial.print("|");
-  Serial.print(mapPOT);
+  Serial.print(map(analogRead(VRx_2), 0, 1023, 0, 180));
   Serial.print("|");
-  Serial.println(SW_state);
+  Serial.print(map(analogRead(VRy_2), 0, 1023, 0, 180));
+  Serial.print("|");
+  Serial.print(map(analogRead(VRx_3), 0, 1023, 0, 180));
+  Serial.print("|");
+  Serial.print(map(analogRead(VRy_3), 0, 1023, 0, 180));
+  Serial.print("|");
+  Serial.print(map(analogRead(POT_1), 0, 678, 0, 180));  
+  Serial.print("|");
+  Serial.print(map(analogRead(POT_2), 0, 670, 0, 180));  
+  Serial.print("|");
+  Serial.print(map(analogRead(POT_3), 0, 650, 0, 180));  
+  Serial.print("|");
+  Serial.print(map(analogRead(POT_4), 0, 115, 0, 180));  
+  Serial.print("|");
+  if(digitalRead(toggle_1) == HIGH){ Serial.print("1");    
+  } else { Serial.print("0");}
+  Serial.print("|");
+  if(digitalRead(toggle_2) == HIGH){ Serial.print("1");    
+  } else { Serial.print("0");}
+  Serial.print("|");
+  if(digitalRead(toggle_3) == HIGH){ Serial.print("1");    
+  } else { Serial.print("0");}
+  Serial.print("|");
+  if(digitalRead(toggle_4) == HIGH){ Serial.print("1");    
+  } else { Serial.print("0");}
+  Serial.print("|");
+  if(digitalRead(toggle_5) == HIGH){ Serial.print("1");    
+  } else { Serial.print("0");}
+  Serial.print("|");
+  if(digitalRead(toggle_6) == HIGH){ Serial.print("1");    
+  } else { Serial.print("0");}
+  Serial.print("|");
+  if(digitalRead(toggle_7) == HIGH){ Serial.print("1");    
+  } else { Serial.print("0");}
+  Serial.print("|");
+  if(digitalRead(toggle_8) == HIGH){ Serial.print("1");    
+  } else { Serial.print("0");}
+  Serial.print("|");
+  if(digitalRead(toggle_9) == HIGH){ Serial.println("1");    
+  } else { Serial.println("0");}
 
   delay(100);
 }
--- index.js
+++ index.js
@@ -4,9 +4,25 @@ const ReadlineParser = require("serialport").ReadlineParser;
 const mqtt = require("mqtt");
 const client = mqtt.connect("mqtt://143.198.182.161:1883");
 
-let lastX = 90;
-let lastY = 90;
-let lastPOT = 0;
+let lastVRx_1 = 90;
+let lastVRy_1 = 90;
+let lastVRx_2 = 90;
+let lastVRy_2 = 90;
+let lastVRx_3 = 90;
+let lastVRy_3 = 90;
+let lastPOT_1 = 0;
+let lastPOT_2 = 0;
+let lastPOT_3 = 0;
+let lastPOT_4 = 0;
+let lastToggle_1 = 0;
+let lastToggle_2 = 0;
+let lastToggle_3 = 0;
+let lastToggle_4 = 0;
+let lastToggle_5 = 0;
+let lastToggle_6 = 0;
+let lastToggle_7 = 0;
+let lastToggle_8 = 0;
+let lastToggle_9 = 0;
 
 client.on("connect", function () {
   console.log("mqtt server conected");
@@ -17,23 +33,85 @@ client.on("connect", function () {
     console.log("serial port open");
   });
   parser.on("data", (data) => {
-    const [X, Y, POT] = data.split("|");
+    const [VRx_1, VRy_1, VRx_2, VRy_2, VRx_3, VRy_3, POT_1, POT_2, POT_3, POT_4, toggle_1, toggle_2, toggle_3, toggle_4, toggle_5, toggle_6, toggle_7, toggle_8, toggle_9 ] = data.split("|");
     
-    if (X != lastX) {
-      console.log("🚀 X", X);
-      client.publish('/FELIA/ROLL', X);
-      lastX = X;
+    if (VRx_1 != lastVRx_1) {
+      client.publish('/FELIA/ROLL', VRx_1);
+      lastVRx_1 = VRx_1;
     }
-    if (Y != lastY) {
-      console.log("🚀 Y", Y);
-      client.publish('/FELIA/YAW', Y);
-      lastY = Y;
+    if (VRy_1 != lastVRy_1) {
+      client.publish('/FELIA/YAW', VRy_1);
+      lastVRy_1 = VRy_1;
     }
-    if (POT != lastPOT) {
-      console.log("🚀 POT", POT);
-      client.publish('/FELIA/ESC', POT);
-      lastPOT = POT;
+    if (VRx_2 != lastVRx_2) {
+      client.publish('/FELIA/RUDDER', VRx_2);
+      lastVRx_2 = VRx_2;
     }
+    if (VRy_2 != lastVRy_2) {
+      client.publish('/FELIA/VRy_2', VRy_2);
+      lastVRy_2 = VRy_2;
+    }
+    if (VRx_3 != lastVRx_3) {
+      client.publish('/FELIA/CAMX', VRx_3);
+      lastVRx_3 = VRx_3;
+    }
+    if (VRy_3 != lastVRy_3) {
+      client.publish('/FELIA/CAMY', VRy_3);
+      lastVRy_3 = VRy_3;
+    }
+    if (POT_1 != lastPOT_1) {
+      client.publish('/FELIA/ESC', POT_1);
+      lastPOT_1 = POT_1;
+    }
+    if (POT_2 != lastPOT_2) {
+      client.publish('/FELIA/POT_2', POT_2);
+      lastPOT_2 = POT_2;
+    }
+    if (POT_3 != lastPOT_3) {
+      client.publish('/FELIA/POT_3', POT_3);
+      lastPOT_3 = POT_3;
+    }
+    if (POT_4 != lastPOT_4) {
+      client.publish('/FELIA/POT_4', POT_4);
+      lastPOT_4 = POT_4;
+    }
+    if (toggle_1 != lastToggle_1) {
+      client.publish('/FELIA/toggle_1', toggle_1);
+      lastToggle_1 = toggle_1;
+    }
+    if (toggle_2 != lastToggle_2) {
+      client.publish('/FELIA/toggle_2', toggle_2);
+      lastToggle_2 = toggle_2;
+    }
+    if (toggle_3 != lastToggle_3) {
+      client.publish('/FELIA/toggle_3', toggle_3);
+      lastToggle_3 = toggle_1;
+    }
+    if (toggle_4 != lastToggle_4) {
+      client.publish('/FELIA/toggle_4', toggle_4);
+      lastToggle_4 = toggle_4;
+    }
+    if (toggle_5 != lastToggle_5) {
+      client.publish('/FELIA/toggle_5', toggle_5);
+      lastToggle_5 = toggle_5;
+    }
+    if (toggle_6 != lastToggle_6) {
+      client.publish('/FELIA/toggle_6', toggle_6);
+      lastToggle_6 = toggle_6;
+    }
+    if (toggle_7 != lastToggle_7) {
+      client.publish('/FELIA/toggle_7', toggle_7);
+      lastToggle_7 = toggle_7;
+    }
+    if (toggle_8 != lastToggle_8) {
+      client.publish('/FELIA/toggle_8', toggle_8);
+      lastToggle_8 = toggle_8;
+    }
+    if (toggle_9 != lastToggle_9) {
+      client.publish('/FELIA/toggle_9', toggle_9);
+      lastToggle_9toggle_9 = toggle_9;
+    }
+
   });
   
 });
